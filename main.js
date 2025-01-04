e7c26f3bdba99b2d31da205c200e421ed9951714
Sebastiaan Marynissen <sebastiaan.marynissen@gmail.com>
2025-01-04 17:18:26 +0100

    Fix startup process

--- main.js
+++ main.js
@@ -2,18 +2,22 @@ import { app, dialog, BrowserWindow } from 'electron/main';
 import cp from 'node:child_process';
 import path from 'node:path';
 
-if (process.defaultApp) {
-	if (process.argv.length >= 2) {
-		app.setAsDefaultProtocolClient('sc4pac', process.execPath, [path.resolve(process.argv[1])]);
-		// app.removeAsDefaultProtocolClient('sc4pac', process.execPath, [path.resolve(process.argv[1])]);
-	}
-} else {
+// IMPORTANT! The root directory of the application - which is where the 
+// "webapp" and "cli" directories can be found - is not simply process.cwd()! 
+// Apparently when launching sc4p:// urls from a browser, windows sets 
+// C:\Windows\System32 as cwd.
+let root = process.defaultApp ? import.meta.dirname : path.dirname(process.execPath);
+
+// Register the application as default handler for the sc4pac protocol, but only 
+// when packaged.
+if (!process.defaultApp) {
 	app.setAsDefaultProtocolClient('sc4pac');
 }
 
 // Ensure only a single app instance can be running.
 const gotTheLock = app.requestSingleInstanceLock();
 if (!gotTheLock) {
+	console.log('App already running, quitting');
 	app.quit();
 } else {
 	app.on('second-instance', (event, commandLine, cwd) => {
@@ -22,16 +26,56 @@ if (!gotTheLock) {
 	});
 }
 
-// Launches the sc4pac server.
+// Launch the sc4pac server.
+let server;
 async function sc4pac() {
-	let cmd = process.platform === 'win32' ? 'cli/sc4pac.bat' : 'cli/sc4pac';
-	cp.spawn(cmd, [
+	let cmd = process.platform === 'win32' ? 'cli\\sc4pac.bat' : './cli/sc4pac';
+	server = cp.spawn(cmd, [
 		'server',
 		'--port 51515',
 		'--web-app-dir webapp',
-	], { shell: true });
+		'--profiles-dir profiles',
+		'--auto-shutdown=true',
+	], {
+		shell: true,
+		cwd: root,
+	});
+	let { promise, resolve, reject } = Promise.withResolvers();
+	let resolved = false;
+	server.stdout.on('data', async chunk => {
+		if (String(chunk).match(/listening on port/)) {
+
+			// For some reason flutter_bootstrap.js isn't loaded the first time, 
+			// so we'll "warm it up" here.
+			try {
+				await fetch(
+					'http://localhost:51515/webapp/flutter_bootstrap.js',
+				);
+			} finally {
+				resolved = true;
+				resolve(server);
+			}
+		}
+	});
+
+	// If something goes wrong during startup of the sc4pac server, then we have 
+	// to quit the application.
+	server.stderr.on('data', chunk => {
+		console.log(chunk+'');
+		if (!resolved) {
+			dialog.showErrorBox('ERROR', String(chunk));
+			reject();
+		}
+	});
+	return promise;
 }
 
+// All windows closed? Then close the application as well.
+app.on('window-all-closed', () => {
+	app.quit();
+});
+
+// Perform the actual app startup logic now.
 Promise.all([
 	app.whenReady(),
 	sc4pac(),
@@ -41,8 +85,17 @@ Promise.all([
 	if (process.defaultApp) {
 		args = args.slice(1);
 	}
-	await new Promise(cb => setTimeout(cb, 1000));
-	await openUrl(args[0]);
+	if (args.length > 0) {
+		await new Promise(cb => setTimeout(cb, 1000));
+		await openUrl(args[0]);
+	}
+}).catch(() => {
+
+	// If something went wrong during startup, we have to make sure to quit, 
+	// because another application instance is not allowed to be started due to 
+	// the `requestSingleInstanceLock()` call!
+	app.quit();
+
 });
 
 let mainWindow;
